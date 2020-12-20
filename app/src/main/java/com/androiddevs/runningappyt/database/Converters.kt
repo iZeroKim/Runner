7654e5d1b555ca61eddd808b526bdd0853a89de5
iZeroKim <ayana518@gmail.com>
2020-12-20 15:29:56 +0300

    Create ImageToByte converter

---
+++ app/src/main/java/com/androiddevs/runningappyt/database/Converters.kt
@@ -0,0 +1,19 @@
+package com.androiddevs.runningappyt.database
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.room.TypeConverter
+import java.io.ByteArrayOutputStream
+
+class Converters {
+    @TypeConverter
+    fun fromBitmap(bitmap: Bitmap): ByteArray {
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        return outputStream.toByteArray()
+    }
+    @TypeConverter
+    fun toBitmap(bytes: ByteArray): Bitmap{
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
+}
