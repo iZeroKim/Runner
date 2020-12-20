dc64163dcaa69ddf706a9dff5585f01837bc326e
iZeroKim <ayana518@gmail.com>
2020-12-20 19:15:32 +0300

    Enable logging with the timber library

--- app/src/main/java/com/androiddevs/runningappyt/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/MainActivity.kt
@@ -9,12 +9,9 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @Inject
-    lateinit var runDao: RunDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("runDao", "RUNDAO: ${runDao.hashCode()}")
     }
 }
