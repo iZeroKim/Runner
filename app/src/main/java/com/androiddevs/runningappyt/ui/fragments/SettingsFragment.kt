60545bcffef26770fc527e5bf3230c74721ea1ff
iZeroKim <ayana518@gmail.com>
2020-12-20 20:22:24 +0300

    Setup app architecture

---
+++ app/src/main/java/com/androiddevs/runningappyt/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.runningappyt.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.runningappyt.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SettingsFragment: Fragment(R.layout.fragment_settings) {
+}
