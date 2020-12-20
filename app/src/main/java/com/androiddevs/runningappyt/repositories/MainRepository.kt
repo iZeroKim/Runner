2b61a4ff05ad0fc1c323d3fc7f8dcf52d2ef04b4
iZeroKim <ayana518@gmail.com>
2020-12-20 19:28:41 +0300

    Create repository

---
+++ app/src/main/java/com/androiddevs/runningappyt/repositories/MainRepository.kt
@@ -0,0 +1,33 @@
+package com.androiddevs.runningappyt.repositories
+
+import com.androiddevs.runningappyt.database.Run
+import com.androiddevs.runningappyt.database.RunDao
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainRepository @Inject constructor(
+    val runDao: RunDao
+) {
+    suspend fun insertRun(run: Run) = runDao.insertRun(run)
+
+    suspend fun deleteRun(run: Run) = runDao.deleteRun(run)
+
+    fun getAllRunsSortedByDate() = runDao.getAllRunsSortedByDate()
+
+    fun getAllRunsSortedByDistance() = runDao.getAllRunsSortedByDistance()
+
+    fun getAllRunsSortedByAvgSpeed() = runDao.getAllRunsSortedByAvgSpeed()
+
+    fun getAllRunsSortedByCaloriesBurnt() = runDao.getAllRunsSortedByCaloriesBurnt()
+
+    fun getAllRunsSortedByTimeInMillis() = runDao.getAllRunsSortedByTimeInMillis()
+
+    fun getTotalTimeInMillis() = runDao.getTotalTimeInMillis()
+
+    fun getTotalDistance() = runDao.getTotalDistance()
+
+    fun getTotalCalorieBurnt() = runDao.getTotalCaloriesBurnt()
+
+    fun getAvgSpeed() = runDao.getAverageSpeed()
+}
