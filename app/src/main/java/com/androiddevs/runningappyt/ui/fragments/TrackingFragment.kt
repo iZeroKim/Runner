5b09f1230dcbe1ea83dc1763172a698a031e4114
iZeroKim <ayana518@gmail.com>
2020-12-22 18:09:50 +0300

    Draw running  track on map

--- app/src/main/java/com/androiddevs/runningappyt/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/fragments/TrackingFragment.kt
@@ -7,9 +7,14 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.androiddevs.runningappyt.R
 import com.androiddevs.runningappyt.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.androiddevs.runningappyt.other.Constants.POLYLINE_COLOR
+import com.androiddevs.runningappyt.other.Constants.POLYLINE_WIDTH
+import com.androiddevs.runningappyt.services.Polyline
+import com.androiddevs.runningappyt.services.Polylines
 import com.androiddevs.runningappyt.services.TrackingService
 import com.androiddevs.runningappyt.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.PolylineOptions
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
@@ -17,6 +22,9 @@ import kotlinx.android.synthetic.main.fragment_tracking.*
 class TrackingFragment: Fragment(R.layout.fragment_tracking) {
     private val viewModel: MainViewModel by viewModels()
     private var map: GoogleMap?  = null
+    private var isTracking = false
+    private var pathPoints = mutableListOf<Polyline>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mapView?.onCreate(savedInstanceState)
@@ -25,6 +33,29 @@ class TrackingFragment: Fragment(R.layout.fragment_tracking) {
         }
         mapView?.getMapAsync {
             map = it
+        }
+    }
+
+    private fun addAllPolylines(){
+        for (polyline in pathPoints){
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .addAll(polyline)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    private fun addLatestPolyline(){
+        if(pathPoints.isNotEmpty() && pathPoints.last().size > 1){
+            val preLastLatLng = pathPoints.last()[pathPoints.last().size - 2]
+            val lastLatLng = pathPoints.last().last()
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .add(preLastLatLng)
+                .add(lastLatLng)
+            map?.addPolyline(polylineOptions)
         }
     }
 
