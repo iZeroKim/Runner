69821cddba31da5707f2454b91b1f39454903761
iZeroKim <ayana518@gmail.com>
2020-12-22 15:50:24 +0300

    Make tracking service a foreground service

--- app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.androiddevs.runningappyt.ui
 
+import android.content.Intent
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,7 @@ import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.runningappyt.R
+import com.androiddevs.runningappyt.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        navigateToTrackingFragmentIfNeeded(intent)
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
         /** Show bottom navigation on specific fragments only
@@ -28,6 +30,16 @@ class MainActivity : AppCompatActivity() {
 
                 else -> bottomNavigationView.visibility = View.GONE
             }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?){
+        if (intent?.action == ACTION_SHOW_TRACKING_FRAGMENT ){
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
         }
     }
 }
